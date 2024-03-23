87777969065f6b3ff49fb4481fd2ee7c2899aafe
nthr3ads <ebxeax@hotmail.com>
2024-03-23 11:46:14 +0800

    run flash script

--- cthreadpool.cpp
+++ cthreadpool.cpp
@@ -3,12 +3,21 @@
 #include <cerrno>
 #include <stdio.h>
 #include <time.h>
+#include <cstdlib>
+#include <stdint.h>
 
 void *thread_routine(void *arg);
 
 void cthreadpool_init(cthreadpool_t *tp, int thread_nums)
 {
-    
+
 }
 void cthreadpool_add_task(cthreadpool_t *tp, void *(*run)(void *args), void *arg);
 void cthreadpool_destroy(cthreadpool_t *tp);
+
+void *thread_routine(void *arg)
+{
+    struct timespec abs;
+    bool timeout;
+    printf("Info: create thread, and the thread id is: %ld\n", (u_int64_t) pthread_self());
+}
